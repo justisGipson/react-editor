2e934eb297f5debd093d8735d57f16df30f2d678
Justis <jgipson@codelicious.com>
2020-11-17 11:44:23 -0500

    inital commit - react editor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import AceEditor from "react-ace";
+import Terminal from "xterm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/snippets/javascript";
+import "ace-builds/src-noconflict/theme-monokai";
+
+// import "node_modules/xterm/css/xterm.css";
+
+// function onChange(newVal) {
+//   console.log('change', newVal)
+// }
+
+let term = new Terminal();
+term.open(document.getElementById("terminal"));
+term.write("Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ");
+export default class Main extends Component {
+  render() {
+    return (
+      <div>
+        <AceEditor
+          mode="javascript"
+          theme="monokai"
+          height="750px"
+          width="1300px"
+          // onChange={ onChange }
+          name="SOMETHING"
+          editorProps={{ $blockScrolling: true, $blockSelectEnabled: true }}
+          setOptions={{
+            enableBasicAutocompletion: true,
+            enableLiveAutocompletion: true,
+            enableSnippets: true,
+          }}
+        />
+        <div id="terminal">
+          let term = new Terminal();
+          term.open(document.getElementById("terminal")); term.write("Hello from
+          \x1B[1;3;31mxterm.js\x1B[0m $ ");
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
